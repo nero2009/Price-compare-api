b0892b7fda14abe7e6dca4bf742add5608fd69f1
Adaware Nero <neroadaware@gmail.com>
2024-04-28 14:12:34 +0100

    move logic to service

--- internal/handlers/api.go
+++ internal/handlers/api.go
@@ -2,9 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/go-chi/chi"
 	chimiddle "github.com/go-chi/chi/middleware"
@@ -12,7 +10,6 @@ import (
 	"github.com/gorilla/schema"
 	"github.com/nero2009/pricecompare/api"
 	"github.com/nero2009/pricecompare/internal/cache"
-	log "github.com/sirupsen/logrus"
 )
 
 var decoder *schema.Decoder = schema.NewDecoder()
@@ -24,7 +21,7 @@ func Handler(r *chi.Mux, cache *cache.Cache) {
 	r.Route("/api", func(r chi.Router) {
 		r.Get("/products", func(w http.ResponseWriter, r *http.Request) {
 			//get query params
-			var products api.ProductResponse
+			// var products api.ProductResponse
 
 			var queryParams = api.ProductParams{}
 			err := decoder.Decode(&queryParams, r.URL.Query())
@@ -34,43 +31,18 @@ func Handler(r *chi.Mux, cache *cache.Cache) {
 				w.WriteHeader(http.StatusBadRequest)
 				return
 			}
-			fmt.Printf("%v", queryParams)
-
-			cachedProducts, isPresent := cache.Get("products")
-
-			if isPresent {
-				json.Unmarshal(cachedProducts.([]byte), &products)
-				w.Header().Set("Content-Type", "application/json")
-				w.WriteHeader(http.StatusOK)
-				json.NewEncoder(w).Encode(products)
-				return
-			}
-
-			kongaProducts := GetProductsFromKonga()
-			jumiaProducts := GetJumiaProducts()
-
-			products = api.ProductResponse{
-				JumiaProducts: jumiaProducts,
-				KongaProducts: kongaProducts,
-			}
-
-			jsonProducts, err := json.Marshal(products)
+			p, err := GetProducts(queryParams.Product, cache)
 
 			if err != nil {
-				log.Error("Error marshalling the products", err)
 				w.Header().Set("Content-Type", "application/json")
 				w.WriteHeader(http.StatusBadRequest)
 				return
 			}
 
-			//cache the products in redis
-
-			cache.Set("products", jsonProducts, 10*time.Minute)
-
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
-			json.NewEncoder(w).Encode(products)
 
+			json.NewEncoder(w).Encode(p)
 		})
 	})
 }
--- internal/handlers/get-products.go
+++ internal/handlers/get-products.go
@@ -1,18 +1,31 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 	"github.com/nero2009/pricecompare/api"
+	"github.com/nero2009/pricecompare/internal/cache"
 	log "github.com/sirupsen/logrus"
 )
 
-func GetJumiaProducts() []api.Product {
-	url := "https://www.jumia.com.ng/catalog/?q=laptop"
+func GetJumiaProducts(query string) []api.Product {
+	//check if the product is in the cache
+	//if it is, return the product
+	baseURL, err := url.Parse("https://www.jumia.com.ng/catalog")
+	if err != nil {
+		log.Error("Error parsing the URL", err)
+		return nil
+	}
+	queryParams := url.Values{}
+	queryParams.Set("q", query)
+	baseURL.RawQuery = queryParams.Encode()
 
-	response, err := http.Get(url)
+	response, err := http.Get(baseURL.String())
 
 	if err != nil {
 		log.Error("Error fetching the URL", err)
@@ -44,16 +57,20 @@ func GetJumiaProducts() []api.Product {
 
 		fmt.Println(id)
 	})
-
 	return products
-
 }
 
-func GetProductsFromKonga() []api.Product {
-	url := "https://kara.com.ng/catalogsearch/result/?q=laptop"
+func GetProductsFromKonga(query string) []api.Product {
+	baseURL, err := url.Parse("https://kara.com.ng/catalogsearch/result")
+	if err != nil {
+		log.Error("Error parsing the URL", err)
+		return nil
+	}
+	queryParams := url.Values{}
+	queryParams.Set("q", query)
+	baseURL.RawQuery = queryParams.Encode()
 
-	response, err := http.Get(url)
-
+	response, err := http.Get(baseURL.String())
 	if err != nil {
 		log.Error("Error fetching the URL", err)
 	}
@@ -68,6 +85,7 @@ func GetProductsFromKonga() []api.Product {
 
 	if err != nil {
 		log.Error("Error parsing HTML", err)
+		return nil
 	}
 
 	var products []api.Product
@@ -87,5 +105,34 @@ func GetProductsFromKonga() []api.Product {
 	})
 
 	return products
+}
 
+func GetProducts(query string, cache *cache.Cache) (api.ProductResponse, error) {
+	var products api.ProductResponse
+
+	cachedProducts, isPresent := cache.Get(query)
+
+	if isPresent {
+		json.Unmarshal(cachedProducts.([]byte), &products)
+		return products, nil
+	}
+
+	kongaProducts := GetProductsFromKonga(query)
+	jumiaProducts := GetJumiaProducts(query)
+
+	products = api.ProductResponse{
+		JumiaProducts: jumiaProducts,
+		KongaProducts: kongaProducts,
+	}
+
+	jsonProducts, err := json.Marshal(products)
+
+	if err != nil {
+		log.Error("Error marshalling the products", err)
+		return api.ProductResponse{}, err
+	}
+
+	//cache the products in redis
+	cache.Set(query, jsonProducts, 10*time.Minute)
+	return products, nil
 }
